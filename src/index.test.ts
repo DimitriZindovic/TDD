57d3b8c23c46e6977dd1d19d9060319d6974f826
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-10-17 16:08:33 +0200

    Adds cart functionality with total calculation

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,9 +3,15 @@ describe("Cart - total", () => {
     const cart = new Cart();
     expect(cart.getTotal()).toBe(0);
   });
+
+  it("devrait totaliser le prix d’un seul produit", () => {
+    const cart = new Cart();
+    cart.addProduct({ name: "Livre", price: 25 });
+    expect(cart.getTotal()).toBe(25);
+  });
 });
 
-class Cart {
+export class Cart {
   getTotal(): number {
     return 0;
   }
